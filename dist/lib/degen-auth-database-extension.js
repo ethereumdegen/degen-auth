7396aca067aa8182dced432ce48aa0a4e631bd5b
apex mfer <apexmfer@gmail.com>
2022-05-09 13:18:51 -0400

    module

--- dist/lib/degen-auth-database-extension.js
+++ dist/lib/degen-auth-database-extension.js
@@ -1,11 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AuthenticationTokenSchema = exports.ChallengeTokenSchema = exports.AuthUserSchema = void 0;
+exports.AuthenticationTokenSchema = exports.ChallengeTokenSchema = void 0;
 const extensible_mongoose_1 = require("extensible-mongoose");
 const mongoose_1 = require("mongoose");
-exports.AuthUserSchema = new mongoose_1.Schema({
-    publicAddress: { type: String, index: true, unique: true },
-});
 exports.ChallengeTokenSchema = new mongoose_1.Schema({
     challenge: { type: String },
     publicAddress: { type: String, index: true, unique: true },
@@ -22,7 +19,6 @@ class DegenAuthExtension extends extensible_mongoose_1.DatabaseExtension {
     }
     getBindableModels() {
         return [
-            { tableName: 'authusers', schema: exports.AuthUserSchema },
             { tableName: 'challengetokens', schema: exports.ChallengeTokenSchema },
             { tableName: 'authenticationtokens', schema: exports.AuthenticationTokenSchema }
         ];
